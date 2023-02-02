c9f230a00cb4f9106f57b8e1477041b17482c3e8
kanungle <nkanungo@tibco.com>
2023-02-01 19:04:47 -0800

    updated comments

--- Export Shapefile/Export Shapefile.R
+++ Export Shapefile/Export Shapefile.R
@@ -1,16 +1,24 @@
 geometry <- geotable$Geometry
-attributes_df <- subset(geotable, select = -c(Geometry))
+attributes.df <- subset(geotable, select = -c(Geometry))
 export_path <- "C:/users/nkanungo/desktop/shapefile" 
 poly.type <- "polygons"
 proj4<-NA
 #---------------------------
+# Neil Kanungo
+# TIBCO Software
+# February 2023
+# Export Shapefile
+
+
+# Required Input Parameters: geometry, attributes.df, export_path, poly.type
+# Optional Input Parameters: proj4
 
 # Load Libraries
 library(wkb)
 library(rgdal)
 library(sp)
 
-# Set export paths for shapefile and layername
+# Set export paths for shapefile and layer name
 export_path <- gsub("/", "\\\\", export_path)
 if (substr(export_path, nchar(export_path), nchar(export_path)) == "\\") {
   export_path <- paste0(export_path, "Spotfire Output.SHP")
@@ -28,11 +36,11 @@ wkb.data <- readWKB(geometry, proj4string=CRS(proj4))
 
 # Create Spatial Data Frame
 if (grepl("POLYGON", poly.type, fixed = TRUE)){
-  spdf <- SpatialPolygonsDataFrame(wkb.data, data = data.frame(attributes_df))
+  spdf <- SpatialPolygonsDataFrame(wkb.data, data = data.frame(attributes.df))
 } else if (grepl("LINE", poly.type, fixed = TRUE)){
-  spdf <- SpatialLinessDataFrame(wkb.data, data = data.frame(attributes_df))
+  spdf <- SpatialLinessDataFrame(wkb.data, data = data.frame(attributes.df))
 } else if (grepl("POINT", poly.type, fixed = TRUE)){
-  spdf <- SpatialPointsDataFrame(wkb.data, data = data.frame(attributes_df))
+  spdf <- SpatialPointsDataFrame(wkb.data, data = data.frame(attributes.df))
 } else {
   print("SHAPE TYPE NOT RECOGNIZED.")
 }
